078604542e611b49c2a19db7c4982fe443c1410b
Ashish <ashishmurkar23@gmail.com>
2026-01-15 16:02:37 +0530

    add variable.tf and update .gitignore

---
+++ variable.tf
@@ -0,0 +1,21 @@
+variable "subscription_id" {
+  description = "The Subscription ID for the Azure resources."
+  type        = string
+}
+
+variable "client_id" {
+  description = "The Client ID for the Azure Service Principal."
+  type        = string
+}
+
+variable "client_secret" {
+  description = "The Client Secret for the Azure Service Principal."
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "The Tenant ID for the Azure Active Directory."
+  type        = string
+}
+
